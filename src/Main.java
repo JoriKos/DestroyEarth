91902285829598ec409cf2b3032bb7ab5a31d52b
Jorikos <26204@ma-web.nl>
2019-02-12 09:01:19 +0100

    added start

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        boolean run = true;
+        String lc;
+        Scanner s = new Scanner(System.in);
+        System.out.println("Type start");
+        String start = s.nextLine();
+        lc = start.toLowerCase();
+
+
+        while(run){
+            System.out.println("Kies één van de twee:");
+        }
+
     }
 }
