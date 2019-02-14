4e3cb35f5fa2629733f695ad7ff5a8a49686266d
Jorikos <26204@ma-web.nl>
2019-02-14 08:47:39 +0100

    restart

--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,10 @@
-import java.util.Scanner;
-
 public class Main {
 
     public static void main(String[] args) {
-        boolean run = true; // variabalen aanmaken
-        boolean start = true;
-        String lc;
-        Scanner s = new Scanner(System.in);
-        System.out.println("Type start");
-        String startgame;
-
-        String[] goedeKeuzes = new String[10];
-        String[] slechteKeuzes = new String[10];
-
-        while(start){
-           startgame = s.nextLine(); // Checkt wat er getypt word
-            lc = startgame.toLowerCase();
-        if(lc.equals("start")){ // Checkt of er start is getypt
-            start = false;
-
-
-            while(run){
-            System.out.println("Kies één van de twee:");
-
-
-        }}
-        else{
-            System.out.println("Type start");
-        }}
-
+        System.out.println("Welkom bij de quiz. Antwoord de volgende vragen om te kijken of je weet wat goed is voor het milieu");
+        int score = 0;
+        String[] vragenGoed = new String[10];
+        String[] vragenSlecht = new String[10];
     }
 
 }
