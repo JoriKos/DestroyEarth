d6abff351d87ee3f84c8eb412747d70db1dd8622
Jorikos <26204@ma-web.nl>
2019-02-14 10:18:37 +0100

    finished my part

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,19 @@ public class Main {
         String[] vragenSlecht = new String[10];
         boolean run = true;
         Scanner s = new Scanner(System.in);
+        String antwoord;
+        int score = 0;
+        vragenGoed[0] = "";
+        vragenGoed[1] = "";
+        vragenGoed[2] = "";
+        vragenGoed[3] = "";
+        vragenGoed[4] = "";
+        vragenGoed[5] = "";
+        vragenGoed[6] = "";
+        vragenGoed[7] = "";
+        vragenGoed[8] = "";
+        vragenGoed[9] = "";
+
 
         System.out.println(" .----------------.  .----------------.  .----------------.  .----------------. \n" +
                 "| .--------------. || .--------------. || .--------------. || .--------------. |\n" +
@@ -19,13 +32,76 @@ public class Main {
                 "| |              | || |              | || |              | || |              | |\n" +
                 "| '--------------' || '--------------' || '--------------' || '--------------' |\n" +
                 " '----------------'  '----------------'  '----------------'  '----------------' " +
-                "\n \n Welkom bij de quiz. Antwoord de volgende vragen om te kijken of je weet wat goed is voor het milieu");
-        int score = 0;
+                "\n \n Welkom bij de quiz. Antwoord de volgende vragen om te kijken of je weet wat goed is voor het milieu" + "\n Type 'meer' of 'minder'");
 
 
         while(run){
-            
+            for (int i = 0; i < vragenGoed.length; i++) {
+                System.out.println("Kies één van de twee:");
+                System.out.println(vragenGoed[i]);
+                System.out.println();
+                antwoord = s.nextLine();
+                antwoord = antwoord.toLowerCase();
+                if(i >= 0 && i <= 3){
+                    if(antwoord.equals("meer")){
+                        score = score + 1;
+                    } else{
+                        score = score - 1;
+                    }
+
+                } else if(i == 4){
+                    if(antwoord.equals("minder")){
+                        score = score + 1;
+                    } else{
+                        score = score - 1;
+                    }
+
+                } else if(i == 5){
+                    if(antwoord.equals("meer")){
+                        score = score + 1;
+                    } else{
+                        score = score - 1;
+                    }
+
+                } else if(i == 6){
+                    if(antwoord.equals("minder")){
+                        score = score + 1;
+                    } else{
+                        score = score - 1;
+                    }
+
+                } else if(i == 7){
+                    if(antwoord.equals("meer")){
+                        score = score + 1;
+                    } else{
+                        score = score - 1;
+                    }
+
+                } else if(i >= 8 && i <= 9){
+                    if(antwoord.equals("minder")){
+                        score = score + 1;
+                    } else{
+                        score = score - 1;
+                    }
+
+                } else if(i == 10){
+                    if(antwoord.equals("meer")){
+                        score = score + 1;
+                    } else{
+                        score = score - 1;
+                    }
+
+                }
+
+            }
+            run = false;
+        }
+        if(score >= 7 && score <= 10){
+            System.out.println("Je bent goed geïnformeerd over het milieu");
+        } else if(score <=7 && score >= 5){
+            System.out.println("Je bent redelijk goed geïnformeerd over het milieu");
+        } else{
+            System.out.println("Je bent niet goed geïnformeerd over het milieu");
         }
     }
-
 }
